09dc28cd71f035fe7c6e2b9ecc1a1f26402e1845
clxx <Carsten.Lindner@gmx.net>
2023-09-05 20:44:31 +0200

    Update mongo_db.py

--- src/json_repr/mongo_db.py
+++ src/json_repr/mongo_db.py
@@ -1,7 +1,7 @@
 import json
 
 
-def init_mongo_db_types():
+def mongo_db_types():
     class MongoDbType:
         def __init__(self, value):
             self.value = value
@@ -9,15 +9,21 @@ def init_mongo_db_types():
         def __repr__(self):
             return f"{self.__class__.__name__}({json.dumps(self.value, ensure_ascii=False)})"
 
+    global ObjectId
+
     class ObjectId(MongoDbType):
         pass
+
+    global NumberInt
 
     class NumberInt(MongoDbType):
         pass
 
     global null
     null = None
+
     global true
     true = True
+
     global false
     false = False
